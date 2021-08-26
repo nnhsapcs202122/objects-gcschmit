579210e210f6d9402c47c653f3d31c0c5cf70801
Geoff Schmit <gcschmit@gmail.com>
2021-08-26 09:34:09 -0500

    add notes on types of errors

--- TurtleDemo.java
+++ TurtleDemo.java
@@ -29,7 +29,7 @@ public class TurtleDemo
         Turtle crush = new Turtle(ocean);
         crush.setPenColor(Color.ORANGE);
         crush.penDown();
-        
+
         /*
          * A method contains statements.
          * 
@@ -40,9 +40,9 @@ public class TurtleDemo
          * When invoking a method, arguments are passed in parentheses (e.g., 50).
          */
         crush.forward(50);      // comment at the end of a line
-        
+
         // this is a single-line comment (like # python)
-        
+
         /*
          * Identifiers are any combination of:
          *      letters
@@ -53,32 +53,55 @@ public class TurtleDemo
          *  These are various identifiers:
          */
         int y, x2, x_y, total$;
-        
+
         // this is not a valid identifier
         //int 2y;
-        
+
         /*
          * Identifiers are case sensitive (case matters).
          * 
          *  These are different identifiers:
          */
         Turtle squirt, SQUIRT, sQUirt;
-        
+
         /*
          * By convention, constants are all uppercase.
          *      Use an underscore to separate words.
          */
         int SPEED_OF_LIGHT = 300000000; // m/s
-        
+
         /*
          * By convention, most other identifiers start with a lowercase letter.
          *      Subsequent words start with an uppercase letter (CamalCase).
          */
         World pacificOcean;
+        
+        /*
+         * This is a compile-time error. (This code doesn't compile.)
+         * 
+         *  These are often called syntax errors.
+         */
+        //crush.forwrd(50);
+        
+        /*
+         * This is a run-time error. (This code compiles and runs,
+         *      but generates an exception. In most cases, the
+         *      program crashes.)
+         */
+        //crush.forward(1 / 0);
+        
+        /*
+         * This is also a run-time error. (This code compiles and runs,
+         *      but it doesn't produce the expected output.)
+         *      
+         *  More specifically, this is a logical error as the turtle
+         *      turns right instead of left.
+         */
+        // have the turtle turn left and move forward 50 steps
+        crush.turn(90);
+        crush.forward(50);
     }
 }
-
-
 
 
 
