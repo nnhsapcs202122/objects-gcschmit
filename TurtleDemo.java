ebbfc3d7980bb2b67d9599f8ed32ebe0970d8809
Geoff Schmit <gcschmit@gmail.com>
2021-09-01 09:42:50 -0500

    add notes on classes, objects, variables

--- TurtleDemo.java
+++ TurtleDemo.java
@@ -99,10 +99,52 @@ public class TurtleDemo
         // have the turtle turn left and move forward 50
         crush.turn(90);
         crush.forward(50);
+    }
+    
+    public static void drawLine()
+    {
+        /*
+         * Objects are entities in a program.
+         *      Objects have attributes.
+         *      Objects are manipulated by invoking methods.
+         *      
+         *  ocean and crush are variables that reference objects.
+         *  
+         *  Classes describe a collection of objects.
+         *      All objects of a class have the same behavior (methods)
+         *          and they have the same type of attributes
+         *          (but they may have different values).
+         *          
+         *  World and Turtle are classes.
+         */
+        World ocean = new World();
         
+        /*
+         * We use the new operator to construct (create, instantiate) an object.
+         * 
+         * The class of the object is specified immediately  after the
+         *      new operator (e.g., Turtle).
+         *  
+         *  If we need to pass additional information to construct the
+         *      object, arguments are specified in parentheses after the
+         *      class (e.g., ocean).
+         */
+        Turtle crush = new Turtle(ocean);
         
-
+        /*
+         * Variables store values to be used later.
+         *      Variables have a type (e.g., int, Turtle), a name (e.g., width, crush),
+         *          and a value (e.g., 20, ???).
+         */
+        int width;      // declaring a varible
+        width = 20;     // assigning a value to a variable
+        int xPos = 50;  // declaring and assigning a value to a variable
     }
 }
+
+
+
+
+
 
 
